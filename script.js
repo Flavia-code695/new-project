2fed069960f1cc28cf0629455866f6d8e55a8f49
Jim <jim>
2026-02-12 14:21:52 +0100

    u shtua permbajtja e order

--- script.js
+++ script.js
@@ -4,3 +4,13 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   alert("Mirësevini në Pizza Palace!");
 });
+//alert tek orders
+const form = document.getElementById("orderForm");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // ndalon rifreskimin e faqes
+
+  alert("Porosia u mor me sukses! 🍕");
+
+  form.reset(); // pastron fushat e formës
+});
